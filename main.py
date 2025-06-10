9b043a8c666e999af007b351e83a07b75f3dc7cd
skywalker0803r <41248543+skywalker0803r@users.noreply.github.com>
2025-06-10 23:16:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 # 允許所有來源進行CORS，因為前端部署在S3，與後端網域不同
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://baseball-0610-frontend.onrender.com"],  # 實際部署時請替換為您的S3前端網域
+    allow_origins=["*"],  # 實際部署時請替換為您的S3前端網域
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
